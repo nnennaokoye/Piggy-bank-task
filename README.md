55f2be306cbad3b4468b28be11e3bae43dcc0cf7
Nnenna Okoye <nnennaokoye001@gmail.com>
2025-03-01 17:06:19 +0100

    piggy-commits

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# Sample Hardhat Project
-
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat ignition deploy ./ignition/modules/Lock.ts
-```
+https://sepolia-blockscout.lisk.com/address/0x6aA627c20dcb561bE8Fbeeff03906905472E2Cb5#code
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,30 +1,41 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@typechain/hardhat";
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
+
 
 dotenv.config();
 
-const BASE_SEPOLIA_URL = process.env.BASE_SEPOLIA_URL || "https://rpc.sepolia-api.lisk.com"; // Default value
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const LISK_SEPOLIA_RPC_URL = process.env.BASE_SEPOLIA_URL;
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
 const BASE_API_KEY = process.env.BASE_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    sepolia: {
-      url: process.env.ALCHEMY_SEPOLIA_API_KEY_URL,
-      accounts: [process.env.ACCOUNT_PRIVATE_KEY || ''],
+    "lisk-sepolia": {
+      url: LISK_SEPOLIA_RPC_URL || "https://rpc.sepolia-api.lisk.com", // Use env variable or default
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`], 
     },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: {
+      "lisk-sepolia": "123", // Blockscout workaround
+    },
+    customChains: [
+      {
+        network: "lisk-sepolia",
+        chainId: 4202,
+        urls: {
+          apiURL: "https://sepolia-blockscout.lisk.com/api",
+          browserURL: "https://sepolia-blockscout.lisk.com",
+        },
+      },
+    ],
   },
   sourcify: {
     enabled: false,
   },
- 
 };
 
 export default config;
